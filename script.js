be9c9b97988ea24c53791bb17a098ee3af943bac
Brian I <brianingram78@gmail.com>
2020-07-12 12:34:46 -0600

    completed generatePassord function in script

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var upCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var lowCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var numArray = ['0','1','2','3','4','5','6','7','8','9'];
-var specialArray = ["!",'@','#','$','%','^','&','*','(',')','=','-','+','_','?','<','>',';',':'];
-var passArray;
+var upCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowCase = "abcdefghijklmnopqrstuvwxyz";
+var numString = "01234567890"
+var specialString = "!@#$%^&*(){}:;<>_+-=";
 
 // Write password to the #password input
 function writePassword() {
@@ -14,7 +13,58 @@ function writePassword() {
   passwordText.value = password;
 }
 
+function generatePassword() {
+  var charList = "";
+  var password = "";
+  var chosen = 'false';
+  var passLength = 0;
+  var randomNum = -1;
 
+  //add Captial letters to array if chosen to
+  chosen = confirm("Do you want Upper Case letters in your password?")
+  if(chosen){
+    charList+= upCase;
+  }
+  console.log(charList);
+
+  //add Lowers case letters if chosen
+  chosen = confirm("Do you want Lower Case letters in your password?")
+  if(chosen){
+    charList += lowCase;
+  }
+  console.log(charList);
+
+  //add numbers if chosen
+  chosen = confirm("Do you want numbers in your password?")
+  if(chosen){
+    charList += numString;
+  }
+  console.log(charList);
+
+  //add special chars if chosen
+  chosen = confirm("Do you want Special Charaters in your password?")
+  if(chosen){
+    charList += specialString;
+  }
+  console.log(charList);
+
+  //get length of password
+  passLength = prompt("How long do you want you password, from 8 to 128 characters")
+  while(length < 8 && prompt > length){
+    length = prompt("you entered " + length + ", please enter a number from 8 to 128")
+  }
+  console.log(length);
+
+  //generate password
+  for (let i = 0; i < passLength; i++) {
+    randomNum = Math.floor(Math.random() * charList.length);
+          console.log("random Num = " + randomNum);
+    password += charList.charAt(randomNum);
+          console.log(password);
+  }
+
+  return password;
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
